5556854db742fc66e1ac7d56d57c3b0dc9cb13a8
superj6 <46981561+superj6@users.noreply.github.com>
2020-01-04 16:28:41 -0800

    Create pico-canary.cpp

---
+++ pico-canary.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cstdlib.h>
+#include <string.h>
+using namespace std;
+#define endl '\n'
+#define pi pair<int, int>
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	system("./vuln");
+	string s;
+	cin >> s;
+	cout << s << endl;
+
+	return 0;
+}
