8bfbaa25d7d44b066267d1efd179743c71b8e0df
superj6 <46981561+superj6@users.noreply.github.com>
2020-01-04 17:08:41 -0800

    Update canary.cpp

--- Pico/canary.cpp
+++ Pico/canary.cpp
@@ -4,14 +4,16 @@
 #include <cstdlib>
 #include <string.h>
 using namespace std;
-#define endl '\n'
+//#define endl '\n'
 #define pi pair<int, int>
 
 int main(){
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	
-	string s = system("/problems/canary_3_257a2a2061c96a7fb8326dbbc04d0328/vuln");
+	system("/problems/canary_3_257a2a2061c96a7fb8326dbbc04d0328/vuln");
+	string s;
+	cin >> s;
 	cout << s << endl;
 
 	return 0;
