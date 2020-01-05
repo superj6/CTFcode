5549ec9c5f26aab023aadcdc1425947c04e3e0e2
superj6 <46981561+superj6@users.noreply.github.com>
2020-01-04 18:05:54 -0800

    Update canary.cpp

--- Pico/canary.cpp
+++ Pico/canary.cpp
@@ -4,14 +4,31 @@
 #include <cstdlib>
 #include <string.h>
 using namespace std;
-//#define endl '\n'
+#define endl '\n'
 #define pi pair<int, int>
+
+string call(string cmd) {
+
+    string data;
+    FILE * stream;
+    const int max_buffer = 256;
+    char buffer[max_buffer];
+
+    stream = popen(cmd.c_str(), "r");
+    if (stream) {
+    while (!feof(stream))
+	    if (fgets(buffer, max_buffer, stream) != NULL)
+	    data.append(buffer);
+	    pclose(stream);
+    }
+    return data;
+}
 
 int main(){
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	
-	string s = system("/problems/canary_3_257a2a2061c96a7fb8326dbbc04d0328/vuln");
+	string s = call("/problems/canary_3_257a2a2061c96a7fb8326dbbc04d0328");
 	cout << s << endl;
 
 	return 0;
